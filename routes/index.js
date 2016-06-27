9c58af4103a686db19618c32865eb9dffa70afe5
ateof <zateof@gmail.com>
2016-06-27 10:07:06 +0800

    首页列表，博客发布，cdn

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,20 @@ var express = require('express');
 var router = express.Router();
 var Post = require('../entities/Post');
 var auth = require('./auth');
-router.get('/posts', (req, res, next) => {
+router.get('/', (req, res, next) => {
+  Post.find(null, 1).then((result) => {
+    res.render('index', {posts: result});
+  }).catch(function (err) {
+    next(err);
+  });
+}).get('/posts', (req, res, next) => {
   var keywords = req.query.keywords;
   var page = parseInt(req.query.page) || 1;
   Post.find(keywords, page).then((result) => {
     res.render('posts', {posts: result});
   }).catch(function (err) {
     next(err);
-  })
+  });
 }).get('/posts/:id', (req, res, next) => {
   Post.findById(req.params.id).then((result) => {
     res.render('post', {post: result});
